b036ad2c455b532d6cc6ae6791e0fe469b0ee0b5
NoxGamingQC <noxgamingqc@gmail.com>
2022-09-30 00:05:13 -0400

    Remove button in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,7 @@
 <br />
 <div class="footer row">
-    <div class="col-md-3">
-    <br />
-    <br />
-        <a class="btn btn-primary disabled" href="/{{app()->getLocale()}}/privacy_policy" disabled>{{trans('general.privacy_policy')}}</a>
-    </div>
-    <div class="col-md-9 text-center">
-        <div style="margin:5%; margin-left:-25%">
+    <div class="col-md-12 text-center">
+        <div style="margin:5%;">
             <h4><a class="text-color" href="tel:819-989-0867">819-989-0867</a></h4>
             <br />
             <h3>&copy{{trans('general.copyright')}}</h3>
