a8514b2fff90acc2b91418af6604125514e7b3a4
NoxGamingQC <noxgamingqc@gmail.com>
2022-09-30 10:38:59 -0400

    Fix store

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -12,11 +12,18 @@ class StoreController extends Controller
     public function index() {
         $items = Items::all();
         $displayedItemCount = Items::getDisplayedItemCount($items);
+        if(Auth::check()) {
+            $isManager = Auth::user()->isManager;
+            $isDev = Auth::user()->isDev;
+        } else {
+            $isManager = false;
+            $isDev = false;
+        }
         return view('store')->with([
             'items' => $items,
             'displayedItemCount' => $displayedItemCount,
-            'isManager' => Auth::user()->isManager,
-            'isDev' => Auth::user()->isDev
+            'isManager' => $isManager,
+            'isDev' => $isDev
         ]);
     }
 }
