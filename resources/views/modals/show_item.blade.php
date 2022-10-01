e2368f1686fef5ec59297225a6b585bfef8e1ef2
NoxGamingQC <noxgamingqc@gmail.com>
2022-09-30 23:41:10 -0400

    Fix x

--- resources/views/modals/show_item.blade.php
+++ resources/views/modals/show_item.blade.php
@@ -18,7 +18,7 @@
                         <p id="showItemDescription" class="text-justify"></p>
                     </div>
                     <div class="col-md-4">
-                        <img id="showItemPicture" width="100%" />
+                        <img id="showItemPicture" width="100%"/>
                     </div>
                     <div class="col-md-12">
                         <hr />
