0f2dceefa6375b856c4a880394556b78cfb7650d
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-03 16:05:21 -0400

    Add logs

---
+++ resources/lang/en-ca/logs.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'logs' => "Logs",
+    'updated_on' => "Updated on",
+    'file_size' => "File size",
+    'download' => "Download",
+    'generate' => "Generate",
+    'no_logs_found' => "No log found",
+    'logs_limit' => "Warning - Only the logs from the last 30 days are stored on the server."
+];
---
+++ resources/lang/fr-ca/logs.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'logs' => "Journeaux",
+    'updated_on' => "Mis à jour le",
+    'file_size' => "Grosseur du fichier",
+    'download' => "Télécharger",
+    'generate' => "Générer",
+    'no_logs_found' => "Aucun journal trouvé",
+    'logs_limit' => "Attention - Seulement les journaux des 30 derniers jours sont stocker sur le serveur."
+];
