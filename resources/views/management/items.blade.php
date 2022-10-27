1a94c5ae9747732f4ba919b0248cc687d6e7d352
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-27 09:08:16 -0400

    Add add button in item management

--- resources/views/management/items.blade.php
+++ resources/views/management/items.blade.php
@@ -1,8 +1,20 @@
-@extends('layouts.app')
+@extends('layouts.app', ['forceTitle' => true])
 @section('title', trans('management.item_management'))
 @section('content')
+@section('forceTitle', false)
+@include('modals.add_item')
 
-
+<div class="row">
+    <div class="col-md-8">
+        <h1>{{trans('management.item_management')}}</h1>
+    </div>
+    <div class="col-md-3 text-right">
+        <h1><button type="button" class=" btn-add-item btn btn-success" data-toggle="modal" data-target="#addItemModal"><i class="fa fa-plus" aria-hidden="true"></i></button></h1>
+    </div>
+    <div class="col-md-12">
+        <hr />
+    </div>
+</div>
 <div class="col-md-12 table-responsive">
     <table class="table table-striped table-hover">
         <tr>
@@ -15,7 +27,6 @@
             <th class="text-center">{{trans('store.isHidden')}}</th>
             <th class="text-center">{{trans('general.view')}}</th>
             <th class="text-center">{{trans('general.edit')}}</th>
-            <th class="text-center">{{trans('general.delete')}}</th>
         </tr>
         @foreach($items as $key => $item)
             <tr>
@@ -23,12 +34,11 @@
                 <td class="limit-text-length" title="{{$item->name}}">{{$item->name}}</td>
                 <td class="limit-text-length" title="{{$item->description}}">{{$item->description}}</td>
                 <td class="text-center">{{$item->price}}</td>
-                <td class="text-center">{{$item->stock}}</td>
-                <td class="text-center">{{$item->isAvailable ? trans('general.yes') : trans('general.no')}}</td>
-                <td class="text-center">{{$item->isHidden ? trans('general.yes') : trans('general.no')}}</td>
+                <td class="text-center {{($item->stock > 0) ? 'success' : 'danger'}}">{{$item->stock}}</td>
+                <td class="text-center {{$item->isAvailable ? 'success' : 'danger'}}">{{$item->isAvailable ? trans('general.yes') : trans('general.no')}}</td>
+                <td class="text-center {{$item->isHidden ? 'danger' : 'success'}}">{{$item->isHidden ? trans('general.yes') : trans('general.no')}}</td>
                 <td class="text-center"><a class="btn btn-primary btn-sm" href="/store/item/{{$item->id}}"><i class="fa fa-eye" area-hidden="true"></i></a></td>
                 <td class="text-center"><a class="btn btn-warning btn-sm" href="/management/item/{{$item->id}}"><i class="fa fa-pencil" area-hidden="true"></i></a></td>
-                <td class="text-center"><a class="btn btn-danger btn-sm disabled" href="#" disabled><i class="fa fa-times" area-hidden="true"></i></a></td>
             </tr>
         @endforeach
     </table>
