3c3727df86ec8d12464311131f3c436c1c1be0c2
NoxGamingQC <noxgamingqc@gmail.com>
2022-11-13 09:00:34 -0500

    Fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,14 @@
-<br />
-<div class="footer row">
-    <div class="col-md-12 text-center">
-        <div style="margin:5%;">
-            <h4><a class="text-color" href="tel:819-989-0867">819-989-0867</a></h4>
-            <br />
-            <h3>&copy{{trans('general.copyright')}}</h3>
-            <br />
-            <h4>{{trans('general.coded_by')}} <a href="https://noxgamingqc.ca">NGST</a>.</h4>
-            <p><small>commit: {{$sourceVersion}}</small></p>
+<footer>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <div style="margin:5%;">
+                <h4><a class="text-color" href="tel:819-989-0867">819-989-0867</a></h4>
+                <br />
+                <h3>&copy{{trans('general.copyright')}}</h3>
+                <br />
+                <h4>{{trans('general.coded_by')}} <a href="https://noxgamingqc.ca">NGST</a>.</h4>
+                <p><small>commit: {{$sourceVersion}}</small></p>
+            </div>
         </div>
     </div>
-</div>
+</footer>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default" style="margin-top: 5%;">
+<nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">
